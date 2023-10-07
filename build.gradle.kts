6914dd5c955a3810a7c1872d1725e1a45ec7fb29
Dmitrii Karagodin <4319788@gmail.com>
2023-10-07 17:12:30 +0300

    Add template project

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:24.1.0")
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    sourceSets {
+        main {
+            java.setSrcDirs(listOf("src/main"))
+            resources.setSrcDirs(listOf("src/resources"))
+        }
+        test {
+            java.setSrcDirs(listOf("src/test"))
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
